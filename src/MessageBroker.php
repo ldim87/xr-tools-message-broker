f739a6d09d49b02a6ce7ebf52550244093e876a5
Admin <support@1000.menu>
2020-02-27 14:25:32 +0100

    Add composer and source

---
+++ src/MessageBroker.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author  Dmitriy Lukin <lukin.d87@gmail.com>
+ */
+
+namespace XrTools;
+
+/**
+ * Message Broker
+ */
+class MessageBroker {
+
+	function __construct(){
+
+	}
+
+	function test(){
+		return 'YAY';
+	}
+
+}
